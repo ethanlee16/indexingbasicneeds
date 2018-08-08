b455e1718218d61bee1807c9523e2ddc04873ca5
Ken Chen <lbkchen@gmail.com>
2018-08-07 23:28:01 -0700

    Adds dummy seeds for Resource.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def create_resources
+  10.times do |i|
+    Resource.create(
+      title: "Resource number #{i}"
+    )
+  end
+end
+
+create_resources
