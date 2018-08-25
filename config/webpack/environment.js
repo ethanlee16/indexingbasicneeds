dc6975e85f891c1a01d13c3ce016aa691d71b937
Ken Chen <lbkchen@gmail.com>
2018-08-25 14:59:05 -0700

    Adds bootstrap and loaders

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,6 +9,29 @@ module: {
       options: {
         presets: [["env", { modules: false }]]
       }
+    },
+    {
+      test: /\.(scss)$/,
+      use: [
+        {
+          loader: "style-loader" // inject CSS to page
+        },
+        {
+          loader: "css-loader" // translates CSS into CommonJS modules
+        },
+        {
+          loader: "postcss-loader", // Run post css actions
+          options: {
+            plugins: function() {
+              // post css plugins, can be exported to postcss.config.js
+              return [require("precss"), require("autoprefixer")];
+            }
+          }
+        },
+        {
+          loader: "sass-loader" // compiles Sass to CSS
+        }
+      ]
     }
   ];
 }
