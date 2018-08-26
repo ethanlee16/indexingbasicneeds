968e1a78944fe5f16c525a557af531b0cb075f92
Ken Chen <lbkchen@gmail.com>
2018-08-25 17:57:36 -0700

    Annotates models.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: resources
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Resource < ApplicationRecord
 end
