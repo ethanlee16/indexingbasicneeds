1b602f86fd38c8b5b5e7e2ee65d321a9e5e803db
tonyanguyen <tonyanguyen@berkeley.edu>
2018-08-25 18:22:52 -0700

    some divs before pulling changes

--- app/javascript/packs/app/components/ResourceList.jsx
+++ app/javascript/packs/app/components/ResourceList.jsx
@@ -3,8 +3,34 @@ import React from 'react';
 class ResourceList extends React.Component {
   renderResources() {
     return this.props.resources.map(resource => (
-      <div>
-        resource.title
+      <div className="Resources-Page">
+        <div className="Tabs"></div>
+        <div className="Search">
+          <div className="TextField"></div>
+          <div className="SearchBtn"></div>
+        </div>
+
+        <div className="Resources">
+          <div className="Resource">
+            <div className="img"> </div>
+            <div className="title">
+              {resource.title}
+            </div>
+            <div className="description">
+              {resource.title}
+            </div>
+            <div className="links">
+              <div className="link">
+                {resource.link}
+              </div>
+              <div className="upvotes">
+                {resource.votes}
+              </div>
+            </div>
+          
+          </div>
+        </div>
+        
       </div>
     ))
   }
