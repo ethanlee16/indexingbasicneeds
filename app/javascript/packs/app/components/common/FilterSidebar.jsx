c495e2b91f52dab9cfb280e0f084c53bb005650c
Ken Chen <lbkchen@gmail.com>
2018-11-27 22:47:33 -0800

    Fix sidebar styling.

--- app/javascript/packs/app/components/common/FilterSidebar.jsx
+++ app/javascript/packs/app/components/common/FilterSidebar.jsx
@@ -5,18 +5,18 @@ class FilterSidebar extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <div>
+      <div className="filter-sidebar">
         {this.props.sections.map((section, i) => {
           return (
             <div key={`section-${i}`}>
               <h3>{section.name}</h3>
-              <FormGroup label="Other Filters" labelFor="text-input">
+              <FormGroup>
                 {section.tags.map(tag => {
                   return (
                     <Checkbox
                       label={tag.name}
                       onChange={this.props.checkTagCallback(tag.id)}
-                      key={`resource-tag-${tag.id}`}
+                      key={`tag-${tag.id}`}
                     />
                   );
                 })}
