acd72a740403b24e212069a184d5d9085ba2bae9
Ken Chen <lbkchen@gmail.com>
2018-11-27 23:39:12 -0800

    Finish filtering backend.

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -18,4 +18,7 @@ class Resource < ApplicationRecord
     has_many :resource_tags, through: :resource_tag_instances
 
     accepts_nested_attributes_for :resource_tag_instances, allow_destroy: true
+
+    # Filter by multiple tags on OR condition
+    scope :by_tags, -> tag_ids { puts tag_ids; joins(:resource_tag_instances).where(resource_tag_instances: { resource_tag_id: tag_ids }) }
 end
