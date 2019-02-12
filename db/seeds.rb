747ce03422af71edc40c5ad29f7ce3781cd9893d
Ken Chen <ken.chen@samsara.com>
2019-02-11 17:06:33 -0800

    Update seeds so we don't duplicate preset tags and categories on reseed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,48 +31,50 @@ end
 
 def create_resource_tags
   # Student filters
-  ResourceTag.create(name: 'Undergraduate', category: ResourceTag::CATEGORIES[:student])
-  ResourceTag.create(name: 'Graduate', category: ResourceTag::CATEGORIES[:student])
-  ResourceTag.create(name: 'Service Workers/Staff', category: ResourceTag::CATEGORIES[:student])
+  ResourceTag.find_or_create_by(name: 'Undergraduate', category: ResourceTag::CATEGORIES[:student])
+  ResourceTag.find_or_create_by(name: 'Graduate', category: ResourceTag::CATEGORIES[:student])
+  ResourceTag.find_or_create_by(name: 'Service Workers/Staff', category: ResourceTag::CATEGORIES[:student])
 
   # Campus filters
-  ResourceTag.create(name: 'On-Campus', category: ResourceTag::CATEGORIES[:campus])
-  ResourceTag.create(name: 'Off-Campus', category: ResourceTag::CATEGORIES[:campus])
+  ResourceTag.find_or_create_by(name: 'On-Campus', category: ResourceTag::CATEGORIES[:campus])
+  ResourceTag.find_or_create_by(name: 'Off-Campus', category: ResourceTag::CATEGORIES[:campus])
 
   # Community Filters
-  ResourceTag.create(name: 'API Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Black Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Commuter Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'International Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'LGBTQ+ Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Muslim Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Parents and Community', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Students with Disabilities', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Transfer Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Underground Scholars', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'Undocumented Students', category: ResourceTag::CATEGORIES[:community])
-  ResourceTag.create(name: 'SSWANA Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'API Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Black Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Commuter Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'International Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'LGBTQ+ Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Muslim Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Parents and Community', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Students with Disabilities', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Transfer Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Underground Scholars', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'Undocumented Students', category: ResourceTag::CATEGORIES[:community])
+  ResourceTag.find_or_create_by(name: 'SSWANA Students', category: ResourceTag::CATEGORIES[:community])
 end
 
 def create_resource_categories
-  ResourceCategory.create!(name: 'Food Support')
-  ResourceCategory.create!(name: 'Housing Support')
-  ResourceCategory.create!(name: 'Economic Support')
-  ResourceCategory.create!(name: 'Emergency Support')
-  ResourceCategory.create!(name: 'Holistic Support')
+  ResourceCategory.find_or_create_by(name: 'Food Support')
+  ResourceCategory.find_or_create_by(name: 'Housing Support')
+  ResourceCategory.find_or_create_by(name: 'Economic Support')
+  ResourceCategory.find_or_create_by(name: 'Emergency Support')
+  ResourceCategory.find_or_create_by(name: 'Holistic Support')
 end
 
 def create_users
-  User.create(
-    first_name: 'Ken',
-    last_name: 'Chen',
-    is_admin: true,
-    email: 'lbkchen@gmail.com',
-    uid: 'lbkchen@gmail.com',
-    provider: 'email',
-    password: 'password',
-    password_confirmation: 'password'
-  )
+  unless User.where(email: 'lbkchen@gmail.com').exists?
+    User.create(
+      first_name: 'Ken',
+      last_name: 'Chen',
+      is_admin: true,
+      email: 'lbkchen@gmail.com',
+      uid: 'lbkchen@gmail.com',
+      provider: 'email',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+  end
 end
 
 puts 'Seeding resources.'
