18a0d7a93ec3fbc41ad6835dfefb5c42acf1d578
Ken Chen <ken.chen@samsara.com>
2019-02-18 21:50:29 -0800

    Fix resource modal link UI.

--- app/javascript/app/components/ResourceIndexPage.jsx
+++ app/javascript/app/components/ResourceIndexPage.jsx
@@ -195,7 +195,11 @@ class ResourceIndexPage extends React.Component {
               }
             />
             {resource.link && (
-              <a href={resource.link} target="_blank" style={{ width: "100%" }}>
+              <a
+                href={resource.link}
+                className="resource-modal-link"
+                target="_blank"
+              >
                 <Button
                   large
                   minimal
