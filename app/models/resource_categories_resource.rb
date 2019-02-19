74d6dfe52ada68ba7490985e4a7180ce44bc590b
Ken Chen <ken.chen@samsara.com>
2019-02-18 20:35:15 -0800

    Annotate resource models.

--- app/models/resource_categories_resource.rb
+++ app/models/resource_categories_resource.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: resource_categories_resources
+#
+#  id                   :bigint(8)        not null, primary key
+#  resource_id          :bigint(8)        not null
+#  resource_category_id :bigint(8)        not null
+#
+
+
 class ResourceCategoriesResource < ApplicationRecord
   belongs_to :resource
   belongs_to :resource_category
