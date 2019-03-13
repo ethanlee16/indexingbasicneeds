e027e1a912dccd5f168e6d210abca9206e2c7be0
Ken Chen <ken.chen@samsara.com>
2019-03-12 21:59:50 -0700

    Annotate rResearchFile.

--- app/models/research_file.rb
+++ app/models/research_file.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: research_files
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  category   :integer          default("campus")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ResearchFile < ApplicationRecord
+  CATEGORIES = {
+    campus: 0,
+    statewide: 1,
+    national: 2
+  }.freeze
+
+  enum category: CATEGORIES
 end
--- test/models/research_file_test.rb
+++ test/models/research_file_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: research_files
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string
+#  category   :integer          default("campus")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ResearchFileTest < ActiveSupport::TestCase
