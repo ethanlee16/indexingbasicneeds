d79064ff7810fdf113d76eb20e1684b2bec8bf15
Ken Chen <ken.chen@samsara.com>
2019-03-12 21:57:45 -0700

    Adds research file model and annotate.

---
+++ app/models/research_file.rb
@@ -0,0 +1,2 @@
+class ResearchFile < ApplicationRecord
+end
---
+++ test/models/research_file_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ResearchFileTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
